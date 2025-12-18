c486a387e997eba16ef06cff7da48b6ff1d28d21
HanJeongWoo <87626378+blackeichi@users.noreply.github.com>
2025-12-18 21:32:48 +0900

    [ADD] 마우스위치에 로딩 컴포넌트 추가 - 라우팅동안 작동하는 로딩

--- components/template/windowTemplate.tsx
+++ components/template/windowTemplate.tsx
@@ -1,13 +1,25 @@
 "use client";
 
+import { useEffect } from "react";
 import WindowTemplateFooter from "../organisms/windowTemplateFooter";
+import { usePathname } from "next/navigation";
+import { useSetAtom } from "jotai";
+import { loadingState } from "@/libs/atom";
+import MouseLoading from "../atoms/mouseLoading";
 
 export const WindowTemplate = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const setLoading = useSetAtom(loadingState);
+  useEffect(() => {
+    setLoading(false);
+  }, [pathname, setLoading]);
   return (
     <main className="relative flex h-full w-full flex-col justify-center overflow-hidden">
       <div className="relative h-full w-full overflow-hidden">{children}</div>
       {/* 윈도우즈 바닥 메뉴바 */}
       <WindowTemplateFooter />
+      {/* 마우스 위치의 로딩 아이콘 - 라우팅하는 동안 보여짐 */}
+      <MouseLoading />
     </main>
   );
 };
